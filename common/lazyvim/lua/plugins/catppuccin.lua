3a7866beef106c01c6f37f2c9cf3baaf7634a9d6
Pierre-Nicolas SORMANI <psormani@anynines.com>
2025-03-18 13:53:28 +0100

    Make LazyVim transparent

---
+++ common/lazyvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
