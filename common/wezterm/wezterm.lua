953988d95cf78964eb2ac278dc87a9863c915246
Ark'Anoryn <3142893+arkanoryn@users.noreply.github.com>
2025-05-01 17:22:34 +0200

    Configuration for WezTerm: use workspaces and panes
    Add shortcuts to use panes and workspaces
    Create workspaces for dotfiles, move and programming on startup

--- common/wezterm/wezterm.lua
+++ common/wezterm/wezterm.lua
@@ -1,6 +1,47 @@
 local wezterm = require("wezterm")
+local keymaps = require("keys")
+local mux = wezterm.mux
 
 -- wezterm.mux.spawn_window { width = 60, height = 30 }
+wezterm.on("update-right-status", function(window, pane)
+	window:set_right_status(window:active_workspace())
+end)
+
+wezterm.on("gui-startup", function(cmd)
+	-- allow `wezterm start -- something` to affect what we spawn
+	-- in our initial window
+	local args = { "/opt/homebrew/bin/fish", "-l" }
+	if cmd then
+		args = cmd.args
+	end
+
+	-- Set a workspace for coding on a current project
+	-- Top pane is for the editor, bottom pane is for the build tool
+	local dotfiles_dir = wezterm.home_dir .. "/.dotfiles"
+	local projects_dir = wezterm.home_dir .. "/projects"
+	local move_dir = wezterm.home_dir .. "/projects/anynines/move"
+
+	mux.spawn_window({
+		workspace = "dotfiles",
+		cwd = dotfiles_dir,
+		args = args,
+	})
+
+	mux.spawn_window({
+		workspace = "programming",
+		cwd = projects_dir,
+		args = args,
+	})
+
+	mux.spawn_window({
+		workspace = "move",
+		cwd = move_dir,
+		args = args,
+	})
+
+	-- We want to startup in the coding workspace
+	mux.set_active_workspace("programming")
+end)
 
 return {
 	-- run fish
@@ -21,18 +62,7 @@ return {
 	adjust_window_size_when_changing_font_size = false,
 
 	-- Keybindings / shortcuts
-	keys = {
-		{
-			key = "/",
-			mods = "CTRL",
-			action = wezterm.action.QuickSelect,
-		},
-		{
-			key = "k",
-			mods = "CMD",
-			action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-		},
-	},
+	keys = keymaps,
 	mouse_bindings = {
 		-- Ctrl-click will open the link under the mouse cursor
 		{
