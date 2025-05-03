257d8eac083be0951bcee5de34c12367f3de4cbb
Ark'Anoryn <3142893+arkanoryn@users.noreply.github.com>
2025-05-03 13:38:33 +0200

    Lazyvim: fix colorscheme
    colorscheme properly loads with transparent background

--- common/lazyvim/lua/plugins/catppuccin.lua
+++ common/lazyvim/lua/plugins/catppuccin.lua
@@ -4,13 +4,8 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
+      flavor = "mocha",
       transparent_background = true,
     },
-
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-
-      vim.cmd.colorscheme("catppuccin")
-    end,
   },
 }
